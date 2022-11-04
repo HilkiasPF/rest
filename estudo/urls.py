ca450112d9e18cf6a06c6ccdd7f50b0d6177dfcb
Hilkias Fernandes <hilkiasf@gmail.com>
2022-11-04 01:25:12 -0300

    views2.0

--- estudo/urls.py
+++ estudo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CursoAPIView, AvaliacaoAPIView
+from .views import CursoAPIView, AvaliacaoAPIView, AvaliacoesAPIView, CursosAPIView
 
 urlpatterns = [
-    path('cursos/', CursoAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+    path('cursos/', CursosAPIView.as_view(), name='cursos'), #Mostrar e criar
+    path('curso/<int:pk>', CursoAPIView.as_view(), name='curso'), #selecionar para deleter ou update
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('avaliacao/<int:pk>', AvaliacaoAPIView.as_view(), name='avaliacao'),
 ]
--- estudo/views.py
+++ estudo/views.py
@@ -5,10 +5,18 @@ from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CrusoSerializer, AvaliacaoSerializer
 
-class CursoAPIView(generics.ListCreateAPIView):
+class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
     serializer_class = CrusoSerializer
 
-class AvaliacaoAPIView(generics.ListCreateAPIView):
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CrusoSerializer
+
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
