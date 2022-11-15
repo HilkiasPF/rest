b68e63f73a4f8e791be92e87e489a9d09a025fb7
Hilkias Fernandes <hilkiasf@gmail.com>
2022-11-15 13:24:27 -0300

    sobrescrever

--- estudo/urls.py
+++ estudo/urls.py
@@ -4,6 +4,8 @@ from .views import CursoAPIView, AvaliacaoAPIView, AvaliacoesAPIView, CursosAPIV
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'), #Mostrar e criar
     path('curso/<int:pk>', CursoAPIView.as_view(), name='curso'), #selecionar para deleter ou update
+    path('curso/<int:curso_pk>/avaliacoes', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    path('curso/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>', AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
     path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
     path('avaliacao/<int:pk>', AvaliacaoAPIView.as_view(), name='avaliacao'),
 ]
